2d95126d15d3ddf07e593bec79ea5b77c8e32aa1
ricky26 <rickytaylor26@gmail.com>
2010-05-10 08:13:27 +0100

    Fully working wm8991 sound support.

--- sound/soc/iphone/iphone-i2s.c
+++ sound/soc/iphone/iphone-i2s.c
@@ -22,6 +22,10 @@
 #include <sound/initval.h>
 #include <sound/soc.h>
 
+#ifdef CONFIG_IPHONE_3G
+#	include "../codecs/wm8991.h"
+#endif
+
 #include <mach/iphone-clock.h>
 #include <mach/hardware.h>
 #include <mach/iphone-dma.h>
@@ -105,15 +109,12 @@ static int iphone_i2s_hw_params(struct snd_pcm_substream *substream,
 				struct snd_pcm_hw_params *params,
 				struct snd_soc_dai *socdai)
 {
+	int ret = 0;
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai_link *dai = rtd->dai;
 	struct iphone_i2s_dma_params* dma_params;
 
 	pr_debug("ENTER iphone_i2s_hw_params\n");
-
-#ifdef CONFIG_IPHONE_3G
-	snd_soc_dai_set_pll(socdai, 0, 0, 0x785fc);
-#endif
 
 	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK)
 	{
@@ -162,7 +163,7 @@ static int iphone_i2s_hw_params(struct snd_pcm_substream *substream,
 			dma_params->i2sController + I2S_RXCOM);    /* 0 = LRCK on */
 	}
 
-	return 0;
+	return ret;
 }
 
 static int iphone_i2s_trigger(struct snd_pcm_substream *substream, int cmd,
--- sound/soc/iphone/iphone-sound.c
+++ sound/soc/iphone/iphone-sound.c
@@ -26,9 +26,208 @@ static int iphone_soc_to_bb_init(struct snd_soc_codec *codec)
 
 static int iphone_wm8991_init(struct snd_soc_codec *codec)
 {
+	int ret = 0;
+
 	printk("WM8991 initialising...\n");
-	return 0;
+
+	if(codec == NULL || codec->write == NULL)
+	{
+		ret = -1;
+		return ret;
+	}
+	
+	ret = codec->write(codec, WM8991_POWER_MANAGEMENT_1, 0x1003);
+	if(ret < 0)
+	{
+		printk("iphone-wm8991: failed to set power mode.\n");
+		return ret;
+	}
+
+	// speaker enabled, VREF bias enabled, Vmid to 2 * 50kOhm (normal mode) 
+	ret = codec->write(codec, WM8991_POWER_MANAGEMENT_1, 0x1003);
+
+	// PLL enable, thermal sensor enable, thermal shutdown enable
+	ret = codec->write(codec, WM8991_POWER_MANAGEMENT_2, 0xe000);
+
+	// Left DAC enable, right DAC enable
+	ret = codec->write(codec, WM8991_POWER_MANAGEMENT_3, 0x0003);
+
+	// Right ADC data is output on right channel, normal i2s format
+	ret = codec->write(codec, WM8991_AUDIO_INTERFACE_1, 0x4010);
+
+	// Right DAC outputs right channel.
+	ret = codec->write(codec, WM8991_AUDIO_INTERFACE_2, 0x4000);
+
+	// Timeout clock disabled
+	// GPIO output clock = SYSCLK
+	// Class D Clock Divider = SYSCLK / 16
+	// BCLK frequency = SYSCLK / 8
+	ret = codec->write(codec, WM8991_CLOCKING_1, 0x01ce);
+
+	// SYSCLK is from PLL output
+	// SYSCLK = PLL / 2
+	ret = codec->write(codec, WM8991_CLOCKING_2, 0x5000);
+
+	// Interface 1 = master, interface 2 = slave, interface 1 selected, ADCLRC rate = BCLK / 32
+	ret = codec->write(codec, WM8991_AUDIO_INTERFACE_3, 0x8020);
+
+	// ADCLRC/GPIO1 pin is GPIO1, DACLRC rate = BCLK / 32
+	ret = codec->write(codec, WM8991_AUDIO_INTERFACE_4, 0x8020);
+
+	// Full DAC volume
+	ret = codec->write(codec, WM8991_LEFT_DAC_DIGITAL_VOLUME, 0xc0);
+	ret = codec->write(codec, WM8991_RIGHT_DAC_DIGITAL_VOLUME, 0x1c0);
+
+	// ADC digital high pass enable
+	ret = codec->write(codec, WM8991_ADC_CTRL, 0x100);
+
+	// Full ADC volume
+	ret = codec->write(codec, WM8991_LEFT_ADC_DIGITAL_VOLUME, 0xc0);
+	ret = codec->write(codec, WM8991_LEFT_ADC_DIGITAL_VOLUME, 0x1c0);
+
+	// GPIO 2 pull down enable, GPIO 2 is IRQ output, GPIO 1 is an input pin
+	ret = codec->write(codec, WM8991_GPIO1_GPIO2, 0x1700);
+
+	// GPIO 4 pull down enable and is input, GPIO 3 is input
+	ret = codec->write(codec, WM8991_GPIO3_GPIO4, 0x1000);
+
+	// GPIO 6 pull down enable and is input, GPIO 5 is input and has IRQ enabled
+	ret = codec->write(codec, WM8991_GPIO5_GPIO6, 0x1040);
+
+	// 4-wire readback mode
+	ret = codec->write(codec, WM8991_GPIOCTRL_2, 0x0);
+
+	// Temperature sensor inverted, gpio 3 polarity inverted
+	ret = codec->write(codec, WM8991_GPIO_POL, 0x0804);
+
+	// All input PGAs to 0dB and muted
+	ret = codec->write(codec, WM8991_LEFT_LINE_INPUT_1_2_VOLUME, 0x008b);
+	ret = codec->write(codec, WM8991_LEFT_LINE_INPUT_3_4_VOLUME, 0x008b);
+	ret = codec->write(codec, WM8991_RIGHT_LINE_INPUT_1_2_VOLUME, 0x008b);
+	ret = codec->write(codec, WM8991_RIGHT_LINE_INPUT_3_4_VOLUME, 0x008b);
+
+	// headphones at -73 dB, zero cross enabled
+	ret = codec->write(codec, WM8991_LEFT_OUTPUT_VOLUME, 0x00b0);
+	ret = codec->write(codec, WM8991_RIGHT_OUTPUT_VOLUME, 0x01b0);
+
+	// Line out muted
+	ret = codec->write(codec, WM8991_LINE_OUTPUTS_VOLUME, 0x0066);
+
+	// out3 and out4 muted
+	ret = codec->write(codec, WM8991_OUT3_4_VOLUME, 0x0022);
+
+	// zero cross enabled, 0 dB 
+	ret = codec->write(codec, WM8991_LEFT_OPGA_VOLUME, 0x00f9);
+	ret = codec->write(codec, WM8991_RIGHT_OPGA_VOLUME, 0x01f9);
+
+	// no speaker output attenuation
+	ret = codec->write(codec, WM8991_SPEAKER_VOLUME, 0x0);
+
+	// speaker amplifier in class AB mode
+	ret = codec->write(codec, WM8991_CLASSD1, 0x103);
+
+	// reserved. data sheet says should be 0x55, but 0x57 on iPhone
+	ret = codec->write(codec, WM8991_CLASSD1 + 1, 0x57);
+
+	// no speaker boost
+	ret = codec->write(codec, WM8991_CLASSD3, 0x0100);
+
+	// speaker volume zero cross enabled, -1 dB
+	ret = codec->write(codec, WM8991_CLASSD3 + 1, 0x01f8);
+
+	ret = codec->write(codec, WM8991_INPUT_MIXER1, 0x0);
+	ret = codec->write(codec, WM8991_INPUT_MIXER2, 0x0);
+	ret = codec->write(codec, WM8991_INPUT_MIXER3, 0x0);
+	ret = codec->write(codec, WM8991_INPUT_MIXER4, 0x0);
+	ret = codec->write(codec, WM8991_INPUT_MIXER5, 0x0);
+	ret = codec->write(codec, WM8991_INPUT_MIXER6, 0x0);
+	ret = codec->write(codec, WM8991_OUTPUT_MIXER1, 0x0);
+	ret = codec->write(codec, WM8991_OUTPUT_MIXER2, 0x0);
+	ret = codec->write(codec, WM8991_OUTPUT_MIXER3, 0x0);
+	ret = codec->write(codec, WM8991_OUTPUT_MIXER4, 0x0);
+	ret = codec->write(codec, WM8991_OUTPUT_MIXER5, 0x0);
+	ret = codec->write(codec, WM8991_OUTPUT_MIXER6, 0x0);
+
+	// analogue bias optimisation to lowest (default)
+	ret = codec->write(codec, WM8991_OUT3_4_MIXER, 0x180);
+
+	ret = codec->write(codec, WM8991_LINE_MIXER1, 0x0);
+	ret = codec->write(codec, WM8991_LINE_MIXER2, 0x0);
+
+	// unmute left and right DAC to SPKMIX
+	ret = codec->write(codec, WM8991_SPEAKER_MIXER, 0x3);
+
+	ret = codec->write(codec, WM8991_ADDITIONAL_CONTROL, 0x0);
+	ret = codec->write(codec, WM8991_ANTIPOP1, 0x0);
+
+	// Enables the VGS / R current generator and the analogue input and output bias
+	ret = codec->write(codec, WM8991_ANTIPOP2, 0x8);
+
+	ret = codec->write(codec, WM8991_MICBIAS, 0x0);
+
+	// PLL fractional mode, N = 7
+	ret = codec->write(codec, WM8991_PLL1, 0x0087);
+
+	// K = 0x85FC = 34300
+	ret = codec->write(codec, WM8991_PLL2, 0x0085);
+	ret = codec->write(codec, WM8991_PLL3, 0x00fc);
+
+	// R = 7.523376465
+	
+	return ret;
 }
+
+#ifdef CONFIG_IPHONE_3G
+static int iphone_wm8991_params(struct snd_pcm_substream *substream,
+	struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *codec_dai = rtd->dai->codec_dai;
+	struct snd_soc_dai *cpu_dai = rtd->dai->cpu_dai;
+	int ret = 0;
+
+	ret = snd_soc_dai_set_fmt(codec_dai,
+		SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+		SND_SOC_DAIFMT_CBM_CFM);
+	if(ret < 0)
+	{
+		printk("iphone-wm8991: failed to set cpu dai format.\n");
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_fmt(cpu_dai,
+		SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+		SND_SOC_DAIFMT_CBM_CFM);
+	if(ret < 0)
+	{
+		printk("iphone-wm8991: failed to set cpu dai format.\n");
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_clkdiv(codec_dai, WM8991_BCLK_DIV, 0x1ce);
+	if(ret < 0)
+	{
+		printk("iphone-wm8991: failed too set BCLK.\n");
+		return ret;
+	}
+
+	ret = snd_soc_dai_set_pll(codec_dai, 0, -1, 0x785fc);
+	if(ret < 0)
+	{
+		printk("iphone-wm8991: failed to set PLL.\n");
+		return ret;
+	}
+
+	return ret;
+}
+
+static struct snd_soc_ops iphone_wm8991_ops = {
+	.hw_params = iphone_wm8991_params,
+	//.hw_free = iphone_wm8991_free,
+};
+
+#endif
+
 
 static struct snd_soc_dai_link iphone_dai_links[] = {
 #ifdef CONFIG_IPHONE_2G
@@ -54,6 +253,7 @@ static struct snd_soc_dai_link iphone_dai_links[] = {
 		.cpu_dai		= &iphone_i2s_wm8758_dai, // This is bad, jah?
 		.codec_dai		= &wm8991_dai,
 		.init			= iphone_wm8991_init,
+		.ops			= &iphone_wm8991_ops,
 	},
 #endif
 };
